4153d260421474d9507c785d341a972daeb2f0ed
Beidysy <beidyasy@gmail.com>
2023-04-08 05:22:38 -0700

    Timer added

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -23,6 +23,34 @@ namespace MatchGame
         public MainWindow()
         {
             InitializeComponent();
+
+            SetUpGame();
+        }
+
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+            {
+                "🐙","🐙",
+                "🐕","🐕",
+                "🐘","🐘",
+                "🐺","🐺",
+                "🦅","🦅",
+                "🐰","🐰",
+                "🐵","🐵",
+                "🐧","🐧",
+            };
+
+            Random random = new Random();
+
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
         }
     }
 }
